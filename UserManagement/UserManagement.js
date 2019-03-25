7f6526f4c49dd3933348e0f91401e9f43b407fbd
HzKharel <43970402+HzKharel@users.noreply.github.com>
2019-03-25 15:03:12 +0000

    Can make various requests regarding the user

--- UserManagement/UserManagement.js
+++ UserManagement/UserManagement.js
@@ -13,20 +13,136 @@ function register(req, res, next) {
   VALUES (?,?,?,?,?,?)`;
     db.run(SQLinsert, [password, username, first_name, last_name, email, date], (err) => {
         if (err) {
-            console.log(err);
+         //   console.log(err);
+            console.log("hol up");
             if (err.toString().toLowerCase().includes('email')) {
                 res.send("ERROR: Email Address Must Be Unique");
             }
-            if (err.toString().toLowerCase().includes('user_name')) {
+            else if (err.toString().toLowerCase().includes('user_name')) {
                 res.send("ERROR: User Name Must Be Unique");
+            }
+            else {
+                res.send("Unexpected Error!");
             }
         }
         else {
             res.send("User Created Successfully");
+           // next();
+
         }
+
 
     });
 
 }
 
+function auth(req, res, next) {
+    const username = req.header('username');
+    const password = req.header('password');
+
+    let sql = `SELECT User_Name, User_Password FROM User WHERE User_Name = "${username}"`;
+    db.get(sql, [], (err, row)=>{
+        if(err){
+            console.log(err);
+            res.sendStatus(400);
+        }
+        else {
+            try{
+                if(row.User_Name === username && row.User_Password === password){
+                    next();
+                }
+                else{
+
+
+                    res.sendStatus(404);
+                }
+            }
+            catch (e){
+                res.sendStatus(404);
+            }
+
+        }
+    });
+}
+
+function updateDetails(req, res, next) {
+
+    const user_name = req.header('username');
+    let first_name = req.body.first_name;
+    let last_name = req.body.last_name;
+    let password = req.body.password;
+    let email = req.body.email;
+
+    let sql = `UPDATE
+                 user
+                    SET
+                        User_Password = "${password}",
+                        First_Name = "${first_name}",
+                        Last_Name = "${last_name}",
+                        Email = "${email}"
+                    WHERE
+                        User_Name = "${user_name}"`;
+
+    db.run(sql, [], (err)=>{
+        if(err){
+            console.log(err);
+            res.send(400);
+        }
+        else {
+            next();
+        }
+    });
+    
+}
+
+function getUserDetails(req, res, next) {
+    const user_name = req.header('username');
+    let sql = `SELECT * from USER where User_Name = "${user_name}"`
+    db.get(sql,[], (err, row)=>{
+        if(err){
+            res.sendStatus(400);
+        }
+        else{
+            const user_id = row.UserID;
+            const user_name = row.User_Name;
+            const user_password = row.User_Password;
+            const first_name = row.First_Name;
+            const last_name = row.Last_Name;
+            const email = row.Email;
+            const creation_date = row.Creation_Date;
+
+            let response = `"User_ID : "${user_id}",
+            "User_Name" : "${user_name}",
+            "User_Password" : "${user_password}",
+            "First_Name" : "${first_name}",
+            "Last_Name" : "${last_name}",
+            "Email" : "${email},
+            "Creation_Date": "${creation_date}"`;
+
+            res.send(response);
+        }
+
+    });
+    
+}
+
+function deleteUser(req, res, next) {
+    const user_name = req.header('username');
+    let sql = `DELETE FROM User WHERE User_Name = "${user_name}"`;
+    db.run(sql, [], (err)=>{
+        if(err){
+            res.sendStatus(400);
+        }
+        else {
+            next();
+        }
+    });
+
+
+}
+
 module.exports.register_user = register;
+module.exports.auth = auth;
+module.exports.update = updateDetails;
+module.exports.user_details = getUserDetails;
+module.exports.delete_user = deleteUser;
--- index.js
+++ index.js
@@ -2,16 +2,32 @@ const express = require('express');
 const user_management = require('./UserManagement/UserManagement');
 const app = express();
 app.use(express.json());
-let user_register = user_management.register_user;
-//request mapping
-/*app.get('/api/registerUser', user_register ,(req, res) => {
-    res.send('Welcome to Express Web App API');
-});
-*/
+const user_register = user_management.register_user;
+const authorize = user_management.auth;
+const updateDetails = user_management.update;
+const user_details = user_management.user_details;
+const delete_user = user_management.delete_user;
 
+//request mapping
 app.post('/api/registerUser',(req, res) => {
     user_register(req, res);
 });
+
+app.post('/api/login',authorize, (req, res) => {
+    res.sendStatus(200);
+});
+
+app.post('/api/updateUser',authorize, updateDetails, (req, res) => {
+    res.sendStatus(200);
+});
+
+app.get('/api/getUserDetails',authorize, user_details);
+
+app.post('/api/deleteUser',authorize, delete_user, (req, res) =>{
+    res.send(200);
+});
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}.`));
